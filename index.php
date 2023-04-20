b6d9b5cfe9648c9d018d91d06b487ca2e9dce703
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2023-04-19 23:11:04 -0300

    começando

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+    <div>
+        <div>
+            <div>
+                <form action="porcess.php" method="POST">
+                    Nome:<input type="text" name="nome"><br>
+                    Email: <input type="email" name="email"><br>
+                    Senha: <input type="password" name="senha"><br>
+                    <button type="submit">ENVIAR</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
